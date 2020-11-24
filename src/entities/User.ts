2b0e6bb2b91c81a0fdf4d3ab83d8700a60c92a11
Eric Hodges <ehodges2004@gmail.com>
2020-11-24 14:56:31 -0800

    Setup: Initial adding users setup with argon2 and hiding return of sensitive info

---
+++ src/entities/User.ts
@@ -0,0 +1,55 @@
+import * as argon2 from 'argon2';
+import { classToPlain, Exclude } from 'class-transformer';
+import { IsEmail, Length } from 'class-validator';
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm';
+
+@Entity('users')
+export class User extends BaseEntity {
+  constructor(user: Partial<User>) {
+    super();
+    Object.assign(this, user);
+  }
+
+  @Exclude()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string;
+
+  @Index()
+  @Length(3, 255, { message: 'Username must be at least 3 characters long' })
+  @Column({ unique: true })
+  username: string;
+
+  @Exclude()
+  @Column()
+  @Length(6, 255)
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await argon2.hash(this.password);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import trim from './middleware/trim';
+import authRoutes from './routes/auth';
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(trim);
+
+app.get('/', (_, res) => res.send('Yo wtf??'));
+app.use('/api/auth', authRoutes);
+
+app.listen(5000, async () => {
+  console.log('Server running at http://localhost:5000');
+
+  try {
+    await createConnection();
+    console.log('Database connected!');
+  } catch (err) {
+    console.log(err);
+  }
+});
