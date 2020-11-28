0e7abdd8982d6693ed38d124e8ff563bb9eafe32
Eric Hodges <ehodges2004@gmail.com>
2020-11-28 14:07:04 -0800

    Setup: Added username/password validation for case empty

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import * as argon2 from 'argon2';
-import { validate } from 'class-validator';
+import { isEmpty, validate } from 'class-validator';
 import { Request, Response, Router } from 'express';
 import { User } from '../entities/User';
 
@@ -37,6 +37,14 @@ const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
   try {
+    let errors: any = {};
+
+    if (isEmpty(username)) errors.username = 'Username must not be empty';
+    if (isEmpty(password)) errors.password = 'Password must not be empty';
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
     const user = await User.findOne({ username });
 
     if (!user) return res.status(404).json({ error: 'User not found' });
